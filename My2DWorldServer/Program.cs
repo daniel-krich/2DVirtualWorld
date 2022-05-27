edd979c31e74b9e139643b809372bd3bcb31f1e0
zogov123 <danielkrich147@gmail.com>
2022-05-27 11:19:38 +0300

    nlog

--- My2DWorldServer/Program.cs
+++ My2DWorldServer/Program.cs
@@ -7,46 +7,91 @@ using System.Text;
 using System.Text.Json;
 using My2DWorldShared.PacketsIn;
 using My2DWorldServer.Services;
+using NLog;
+using NLog.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Info("Application init.");
 
-builder.Services.AddSingleton<UsersSessionCollection>();
-builder.Services.AddScoped<UserSession>();
-builder.Services.AddScoped<IGameCaller, GameCaller>();
-builder.Services.AddScoped<IGameInformer, GameInformer>();
-builder.Services.AddDbContextFactory<SqlDbContext>();
-
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Configuration.AddJsonFile("hostsettings.json", optional: true);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    builder.Services.AddResponseCompression(options =>
+    {
+        options.EnableForHttps = true;
+        options.Providers.Add<BrotliCompressionProvider>();
+        options.Providers.Add<GzipCompressionProvider>();
+    });
+
+    builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+    {
+        options.Level = CompressionLevel.Fastest;
+    });
+
+    builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+    {
+        options.Level = CompressionLevel.Fastest;
+    });
+
+    builder.Services.AddSingleton<UsersSessionCollection>();
+    builder.Services.AddScoped<UserSession>();
+    builder.Services.AddScoped<IGameCaller, GameCaller>();
+    builder.Services.AddScoped<IGameInformer, GameInformer>();
+    builder.Services.AddDbContextFactory<SqlDbContext>();
+
+    builder.Services.AddRazorPages();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseResponseCompression();
+
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        ServeUnknownFileTypes = true
+    });
+
+    app.UseRouting();
+
+    var webSocketOptions = new WebSocketOptions
+    {
+        KeepAliveInterval = TimeSpan.FromSeconds(15)
+    };
+
+    app.UseWebSockets(webSocketOptions);
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.MapRazorPages();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles(new StaticFileOptions
+catch (Exception exception)
 {
-    ServeUnknownFileTypes = true
-});
-
-app.UseRouting();
-
-var webSocketOptions = new WebSocketOptions
+    logger.Error(exception, "Application setup error.");
+    throw;
+}
+finally
 {
-    KeepAliveInterval = TimeSpan.FromSeconds(15)
-};
-
-app.UseWebSockets(webSocketOptions);
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-app.Run();
+    LogManager.Shutdown();
+}
